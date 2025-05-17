ddf8388a33bdbe45983ec028ce818145083d97f2
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2025-05-17 09:54:06 +0800

    feat: :sparkles: FE | Switching From Tailwindcss to Bootstrapcss & Added Navbar Component

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import BootstrapClient from './components/BootstrapClient';
+import Navbar from './components/navbar';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +23,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Navbar />
         {children}
+        <BootstrapClient />
       </body>
     </html>
   );
