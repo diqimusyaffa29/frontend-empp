6a0e93530f9ae011f70605277709b52e04bc9e8e
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2025-05-23 09:38:48 +0800

    Resolving Conflicts

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -32,90 +32,12 @@ const LoginPage = () => {
           username,
           password,
         }
-<<<<<<< HEAD
-    }, [router])
-=======
       );
->>>>>>> dada0d3a416bd7fe6df2fa7949cea1246264d065
 
       if (response.data.success) {
         // Example: Save token to localStorage
         localStorage.setItem("auth_token", response.data.data.token);
 
-<<<<<<< HEAD
-        try {
-            const response = await axios.post('https://e-mpp.rsisabanjarbaru.com/api/login', {
-            // const response = await axios.post('http://localhost:8000/api/login', {
-                username,
-                password,
-            });
-
-            if (response.data.success) {
-                // Example: Save token to localStorage
-                localStorage.setItem('auth_token', response.data.data.token);
-
-                // Redirect after successful login (adjust path as needed)
-                router.push('/');
-            } else {
-                setError(response.data.message || 'Login failed');
-            }
-        } catch (err) {
-            console.error(err);
-            setError(err.response?.data?.message || 'An error occurred during login');
-        }
-    };
-    return (
-        <div className=''>
-            <section className="vh-100">
-                <div className="container-fluid h-custom">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col-md-9 col-lg-6 col-xl-5">
-                            <Image
-                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                                alt="foto"
-                                className="img-fluid"
-                                height={500}
-                                width={500}
-                            />
-                        </div>
-                        <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                            <form onSubmit={handleLogin}>
-                                <div className="form-outline mb-4">
-                                    <input
-                                        type="text"
-                                        className="form-control form-control-lg"
-                                        placeholder="Enter a valid username"
-                                        value={username}
-                                        onChange={(e) => setUsername(e.target.value)}
-                                        required
-                                    />
-                                </div>
-                                <div className="form-outline mb-3">
-                                    <input
-                                        type="password"
-                                        className="form-control form-control-lg"
-                                        placeholder="Enter password"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        required
-                                    />
-                                </div>
-
-                                {error && <p className="text-danger">{error}</p>}
-
-                                <div className="text-center text-lg-start mt-4 pt-2">
-                                    <button
-                                        type="submit"
-                                        className="btn btn-primary btn-lg"
-                                        style={{ paddingLeft: 50, paddingRight: 50 }}
-                                    >
-                                        Login
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-=======
         // Redirect after successful login (adjust path as needed)
         router.push("/");
       } else {
@@ -161,7 +83,6 @@ const LoginPage = () => {
                     onChange={(e) => setPassword(e.target.value)}
                     required
                   />
->>>>>>> dada0d3a416bd7fe6df2fa7949cea1246264d065
                 </div>
 
                 {error && <p className="text-danger">{error}</p>}
