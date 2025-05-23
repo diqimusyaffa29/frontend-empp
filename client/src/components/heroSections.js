a23e0304f58a75b01bc53ccc8db1a958049d131e
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2025-05-23 10:30:58 +0800

    FE BE | gitignore dist folder and Fixing minor styling assets

--- client/src/components/heroSections.js
+++ client/src/components/heroSections.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const HeroSection = ({title, description}) => {
+    const prefix = process.env.NEXT_PUBLIC_ASSET_PREFIX || '';
     return (
-        <section className="py-5 bg-black text-white text-center mt-5" id='heroSection'>
+        <section className="py-5 bg-black text-white text-center mt-5" id='heroSection' style={{backgroundImage: `url(${prefix}/assets/hero.jpg)`}}>
             <div className="container my-xl-5">
                 <div className="row align-items-center">
                     <p className="display-3">{title}</p>
