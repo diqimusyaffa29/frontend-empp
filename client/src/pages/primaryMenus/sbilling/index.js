acc8cd5bac57984aebea92b96bf90fe6e791b000
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2025-05-30 11:50:35 +0800

    feat: :sparkles: FE | Added Dynamic URL billing, Sarpras and Tutorial Page

--- client/src/pages/primaryMenus/sbilling/index.js
+++ client/src/pages/primaryMenus/sbilling/index.js
@@ -5,7 +5,7 @@ import React from 'react'
 const sbilling = () => {
     return (
         <div className='container' style={{ marginTop: 100 }}>
-            <IconButtonLink imgSrc="/assets/gagalbayar.avif" alt={`Pasien Gagal Bayar`} label={`Pasien Gagal Bayar`} url={`https://docs.google.com/forms/d/e/1FAIpQLSezmtCO4acfwPF4dMz9ayPlkyke-4VCz99BQtB--fLYDChsIQ/viewform`} imgHeight={100} imgWidth={100} fontWeight={`fs-xl-4`}/>
+            <IconButtonLink imgSrc="/assets/gagalbayar.avif" alt={`Pasien Gagal Bayar`} label={`Pasien Gagal Bayar`} url={url?.billing} imgHeight={100} imgWidth={100} fontWeight={`fs-xl-4`}/>
             <Link href={`/`} >
                 <button className="btn btn-primary" type="submit" style={{ float: 'right' }}>Back to Home</button>
             </Link>
